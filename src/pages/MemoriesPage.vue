f8e38f8306516d6afb6b88e77fb16af5bc2a9901
waiscodes <b.wais@outlook.com>
2021-01-07 15:00:05 -0700

    initial commit

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>All Memories</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <ion-item>Playing Hockey</ion-item>
+        <ion-item>Playing Soccer</ion-item>
+        <ion-item>Fighting Bulls</ion-item>
+        <ion-item>San Fransisco</ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+} from "@ionic/vue";
+
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+  },
+};
+</script>
